1773e9c7302699b43f96cb402b9c3d9de8ccba3b
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-02 18:47:49 +0400

    add child_process for build server

---
+++ build.js
@@ -0,0 +1,17 @@
+const { exec } = require('child_process');
+
+console.log('Building application...');
+
+exec('ls -lh', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+    }
+
+    if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+    }
+
+    console.log(`stdout:\n${stdout}`);
+});
