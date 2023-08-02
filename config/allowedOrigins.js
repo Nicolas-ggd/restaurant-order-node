676c34c3d283e2f9134d96b872273240552314ff
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-03 00:53:07 +0400

    update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:5173',
+    'https://order-ggd.netlify.app/',
+    'https://order-ggd.netlify.app',
     'http://localhost:8000/refresh',
     'http://localhost:8000/login',
     'https://corona.lmao.ninja/v2/countries'
